a0704d335b0685fc0bd81b43bfdd07a0defa84e1
CosmoWolf <kosmowolf@gmail.com>
2022-04-24 21:53:35 +0300

    Урок 5. Создали DTO - для восприятия ответа сервера

---
+++ k_1919_2_1/app/src/main/java/com/example/k_1919_2_1/repository/WeatherDTO.kt
@@ -0,0 +1,21 @@
+package com.example.k_1919_2_1.repository
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class WeatherDTO(
+    @SerializedName("fact")
+    val factDTO: FactDTO,
+    @SerializedName("forecast")
+    val forecastDTO: ForecastDTO,
+    @SerializedName("info")
+    val infoDTO: InfoDTO,
+    @SerializedName("now")
+    val now: Int,
+    @SerializedName("now_dt")
+    val nowDt: String
+):Parcelable
+
+
